519ccd4865f6c7c12acad99294d1136fc4a4aa36
xianwenjian_github <xianwenjian0311@126.com>
2020-05-14 01:47:25 +0800

    Update run.suite.py

--- run/run.suite.py
+++ run/run.suite.py
@@ -7,7 +7,7 @@ suite = unittest.TestSuite()
 suite.addTest(unittest.makeSuite(TestLogin))
 
 # 测试报告文件路径
-report_file = "../report/report{}.html".format(time.strftime("%Y%m%d-%H%M%S"))
+report_file = "../report/report.html"
 with open(report_file, "wb") as f:
     # 创建HTMLTestRunner运行器
     runner = HTMLTestRunner(f, title="xwj之TPshop接口自动化测试报告", description="xwj_V1.0")
